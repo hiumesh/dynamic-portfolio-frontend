2686145312cc53dcb052ecffe61f7bc1ad1e543c
Umesh Chandra Dani <daniumeshchandra.se@gmail.com>
2025-03-05 23:14:40 +0530

    updated folder structure

--- app/(public)/portfolio/page.tsx
+++ app/(public)/portfolio/page.tsx
@@ -1,18 +1,10 @@
-import TopNavbar from "@/components/top-navbar";
-import { createClient } from "@/lib/supabase/server";
 import FilterMenu from "./filter-menu";
 import ListContainer from "./list-container";
 import PortfolioContextProvider from "./context";
 
 export default async function Portfolio() {
-  const supabase = createClient();
-
-  const { data, error } = await supabase.auth.getUser();
-
-  const user = data?.user;
   return (
     <>
-      <TopNavbar user={user} />
       <PortfolioContextProvider>
         <FilterMenu />
         <ListContainer />
